0af7753fcd9572e1a58aabf1464c6e5eaa900922
JooChan <14jshin@gmail.com>
2020-01-15 05:20:03 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500, WEBGL);
+}
+
+function draw() {
+  background(205, 105, 94);
+  /*
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  cylinder(50, 100);
+  */
+  rect(0, 200, 10, 50);
+}
