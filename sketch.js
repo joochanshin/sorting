28171c92a38ec4595712dc76f324d9b608a23b24
JooChan <14jshin@gmail.com>
2020-01-16 17:57:42 -0500

    bars work properly

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let check = 0;
 
 function setup() {
   createCanvas(widthC, heightC, WEBGL);
-  slider = createSlider(10, 50, 25);
+  slider = createSlider(10, 100, 25);
   slider.position(10, 10);
   slider.style('width', '80px');
   randNum();
@@ -30,6 +30,9 @@ function draw() {
   for(let i = 1; i <= num; i++){
   	let a = widthC/(2*num);
   	let widthB = 10;
+  	if (num > 50){
+  		widthB = 5;
+  	}
   	rect(((a * i) - (widthB/2)) - (a*num/2), heightC/2-arr[i-1], widthB, arr[i-1]);
   }
 }
@@ -40,30 +43,30 @@ function randNum(){
   for(let i = 0; i < num; i++){
   	heightB = random(100,400)
   	arr[i] = heightB;
-  	console.log(arr[i]);
+  	console.log("randNum: " + arr[i]);
   }
+  arr.length = num;
 }
 
 function runBubble(){
-	arr = bubbleSort(arr);
-	draw();
+	bubbleSort();
+	//loop();
 }
 
-function bubbleSort(values) {
-  var origValues = values.slice();
-  var length = origValues.length - 1;
+function bubbleSort() {
+  var length = arr.length - 1;
   do {
     var swapped = false;
     for(var i = 0; i < length; ++i) {
-      if (origValues[i] > origValues[i+1]) {
-        var temp = origValues[i];
-        origValues[i] = origValues[i+1];
-        origValues[i+1] = temp;
+      if (arr[i] > arr[i+1]) {
+        var temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
         swapped = true;
       }
     }
   }
   while(swapped === true);
   console.log("ran bubble");
-  return origValues;
+  console.log(arr);
 }
