724dd417526faecffe4c9a7956e4140f536060ae
JooChan <14jshin@gmail.com>
2020-01-16 17:06:38 -0500

    got bubblesort button

--- sketch.js
+++ sketch.js
@@ -12,6 +12,8 @@ function setup() {
   slider.position(10, 10);
   slider.style('width', '80px');
   randNum();
+
+  console.log("BubbleSort: " + arr)
 }
 
 function draw() {
@@ -28,7 +30,7 @@ function draw() {
   for(let i = 1; i <= num; i++){
   	let a = widthC/(2*num);
   	let widthB = 10;
-  	rect(((a * i) - (widthB/2)) - (a*num/2), heightC/2-arr[i], widthB, arr[i]);
+  	rect(((a * i) - (widthB/2)) - (a*num/2), heightC/2-arr[i-1], widthB, arr[i-1]);
   }
 }
 
@@ -40,4 +42,28 @@ function randNum(){
   	arr[i] = heightB;
   	console.log(arr[i]);
   }
+}
+
+function runBubble(){
+	arr = bubbleSort(arr);
+	draw();
+}
+
+function bubbleSort(values) {
+  var origValues = values.slice();
+  var length = origValues.length - 1;
+  do {
+    var swapped = false;
+    for(var i = 0; i < length; ++i) {
+      if (origValues[i] > origValues[i+1]) {
+        var temp = origValues[i];
+        origValues[i] = origValues[i+1];
+        origValues[i+1] = temp;
+        swapped = true;
+      }
+    }
+  }
+  while(swapped === true);
+  console.log("ran bubble");
+  return origValues;
 }
