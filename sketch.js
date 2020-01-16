ea1a44fd5d4a9a5ac845bc52ce2b7c3c58e9f22d
JooChan <14jshin@gmail.com>
2020-01-16 15:08:33 -0500

    fixed random number

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 let widthC = 1000;
 let heightC = window.innerHeight-20;
 let slider;
+let arr = [];
+let heightB = 0;
+let num = 0;
+let check = 0;
 
 function setup() {
   createCanvas(widthC, heightC, WEBGL);
   slider = createSlider(10, 50, 25);
   slider.position(10, 10);
   slider.style('width', '80px');
+  randNum();
 }
 
 function draw() {
@@ -16,13 +21,23 @@ function draw() {
   rotateZ(frameCount * 0.01);
   cylinder(50, 100);
   */
-
-  let num = slider.value();
-
+  num = slider.value();
+  if(check !== num){
+  	randNum();
+  }
   for(let i = 1; i <= num; i++){
-  	let heightB = random(100,400);
   	let a = widthC/(2*num);
   	let widthB = 10;
-  	rect(((a * i) - (widthB/2)) - (a*num/2), heightC/2-heightB, widthB, heightB);
+  	rect(((a * i) - (widthB/2)) - (a*num/2), heightC/2-arr[i], widthB, arr[i]);
+  }
+}
+
+function randNum(){
+  num = slider.value();
+  check = num;
+  for(let i = 0; i < num; i++){
+  	heightB = random(100,400)
+  	arr[i] = heightB;
+  	console.log(arr[i]);
   }
 }
